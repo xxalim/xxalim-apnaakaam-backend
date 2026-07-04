724c7c2b2592fb1198105ec300ef9f46b92fc1c8
SALIM KHAN <xxalimboy@gmail.com>
2026-07-04 18:23:31 +0530

    Remove duplicate password utility

--- server/utils/password.js
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-export function hashPassword(password) {
-  return crypto.createHash("sha256").update(password).digest("hex");
-}
-
-export function verifyPassword(password, hashedPassword) {
-  return hashPassword(password) === hashedPassword;
-}
